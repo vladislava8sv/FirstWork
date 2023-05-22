aef0a4c5529dd189e7a189e55d228e4432fb42c9
vladislava8sv <57909332+vladislava8sv@users.noreply.github.com>
2023-05-23 00:24:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 
+
+#Импорт необходимых модулей
+import torch
+from torch import nn
+from torch.utils.data import DataLoader, Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from matplotlib import pyplot as plt
+
+import os
+from os.path import join as pjoin
+import pandas as pd
+from torchvision.io import read_image
